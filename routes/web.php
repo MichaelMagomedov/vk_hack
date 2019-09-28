7ed79dc868693a01bda52b52ceafc8b6ebdb720d
magomedov <magomedov.michael@gmail.com>
2019-09-28 16:06:27 +0300

    Добавили форнтент

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
-Route::get('/client', '\\App\\Clients\\Controllers\\ClientController@create');
+Route::get('/', '\\App\\Clients\\Controllers\\ClientController@create');
 Route::get('/test/{id}', '\\App\\Test\\Controllers\\TestController@getById');
 Route::post('/answer', '\\App\\Test\\Controllers\\AnswerController@save');
