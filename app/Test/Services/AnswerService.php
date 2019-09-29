58736ff838661e7f544550da7e74d04788634ed8
magomedov <magomedov.michael@gmail.com>
2019-09-29 05:51:45 +0300

    Поправил ответ клиентов

--- app/Test/Services/AnswerService.php
+++ app/Test/Services/AnswerService.php
@@ -3,6 +3,7 @@
 namespace App\Test\Services;
 
 use App\Clients\Repositories\ClientRepository;
+use App\Test\Models\Product;
 use App\Test\Repositories\AnswerRepository;
 use App\Test\Repositories\TestResultRepository;
 use Illuminate\Database\Eloquent\Collection;
@@ -40,7 +41,7 @@ final class AnswerService
         $rate = $answers->sum('rate');
         $testId = $answers->get(0)->question->test->id;
         $testResult = $this->testResultRepository->findByTestAndRate($testId, $rate);
-        if(!isset($testResult)){
+        if (!isset($testResult)) {
             $testResult = $this->testResultRepository->findByTest($testId);
         }
         $answerProducts = $answers->pluck('products');
@@ -49,13 +50,14 @@ final class AnswerService
         foreach ($answerProducts as $products) {
             $resultProducts = $resultProducts->merge($products);
         }
-        $randomProduct = null;
+        $productResult = Product::all()->random(1);
         if ($resultProducts->count() !== 0) {
-            $randomProduct = $resultProducts->random(1)->first();
-            $existClient->products()->attach($randomProduct);
+            $productResult = $resultProducts->random(1)->first();
+            $existClient->products()->attach($productResult);
         }
+
         return [
-            'products' => $randomProduct,
+            'products' => $productResult,
             'result' => $testResult,
         ];
     }
