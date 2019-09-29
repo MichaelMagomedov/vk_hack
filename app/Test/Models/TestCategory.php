2053eeb0b1e650150123d13f9032787a8c4b9261
magomedov <magomedov.michael@gmail.com>
2019-09-29 06:20:06 +0300

    Поправил ответ клиентов

--- app/Test/Models/TestCategory.php
+++ app/Test/Models/TestCategory.php
@@ -3,6 +3,7 @@
 namespace App\Test\Models;
 
 use App\Clients\Models\ClientProduct;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use KirschbaumDevelopment\NovaChartjs\Contracts\Chartable;
 use KirschbaumDevelopment\NovaChartjs\Traits\HasChart;
@@ -11,13 +12,25 @@ final class TestCategory extends Model implements Chartable
 {
     use HasChart;
 
+    private static function getParamsForChart(): array
+    {
+        $params = [];
+        for ($i = -10; $i < 10; $i++) {
+            $day = Carbon::now();
+            $day->addDays($i);
+            $params[] = $day->month . "/" . $day->day;
+        }
+        return $params;
+    }
+
     public static function getNovaChartjsSettings(): array
     {
+
         return [
             'type' => 'line',
             'height' => 300,
             'options' => ['responsive' => true, 'maintainAspectRatio' => false],
-            'parameters' => range(0, 31)
+            'parameters' => static::getParamsForChart()
         ];
     }
 
@@ -39,16 +52,26 @@ final class TestCategory extends Model implements Chartable
         $data = [];
         /** @var Product $product */
         foreach ($products as $product) {
+
             $addedLine = [
                 'label' => $product->name,
                 'borderColor' => sprintf('#%06X', mt_rand(0, 0xFFFFFF)),
-                'data' => array_fill(0, 31, 0),
+                'data' => static::getParamsForChart(),
             ];
+
+            for ($i = 0; $i < 20; $i++) {
+                $addedLine['data'][$i] = 0;
+            };
 
             /** @var ClientProduct $clientProduct */
             foreach ($product->clientProducts as $clientProduct) {
-                $dayNumber = $clientProduct->created_at->daysInMonth;
-                $addedLine['data'][$dayNumber] += 1;
+                /** @var Carbon $dayNumber */
+                $dayNumber = $clientProduct->created_at;
+                $date = $dayNumber->month . "/" . $dayNumber->day;
+                $dataIndex = array_search($date, array_keys($addedLine['data']));
+                if ($dataIndex !== false) {
+                    $addedLine['data'][$dataIndex] += 1;
+                }
             }
             $data[] = $addedLine;
         }
