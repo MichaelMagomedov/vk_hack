488ba46c73b650521f14b8c792a379c129a08164
maximanokhin23 <mrmaximanokhin@yandex.ru>
2019-09-29 09:11:58 +0300

    fixes

---
+++ public/precache-manifest.e17c5b4ba26250979639e8a42af3afc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe69034e53a5e506c2b66336a014c6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "391706abfca3688923db",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "e39d172a4b881d11a1fa",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "391706abfca3688923db",
+    "url": "/static/js/2.a8e457b6.chunk.js"
+  },
+  {
+    "revision": "e39d172a4b881d11a1fa",
+    "url": "/static/js/main.4615e29b.chunk.js"
+  },
+  {
+    "revision": "410c688865c08b0cc074",
+    "url": "/static/js/runtime-main.72ea2c7b.js"
+  }
+]);
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,6 @@
 Route::post('/client', '\\App\\Clients\\Controllers\\ClientController@create');
 Route::post('/answer', '\\App\\Test\\Controllers\\AnswerController@save');
 Route::get('/test/{id}', '\\App\\Test\\Controllers\\TestController@getById');
+Route::get('/',function(){
+   return view('index');
+});
