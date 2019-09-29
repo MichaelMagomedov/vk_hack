9bd90993da6bd8c66e92bfaf35a99a6f9fe9fada
maximanokhin23 <mrmaximanokhin@yandex.ru>
2019-09-29 03:27:46 +0300

    fixes

--- app/Test/Repositories/TestRepository.php
+++ app/Test/Repositories/TestRepository.php
@@ -11,7 +11,7 @@ final class TestRepository
     {
         return Test::with([
             'questions' => function($query) {
-		        $query->orderBy('is_firsts');
+		        $query->orderBy('is_first');
 	    },
             'questions.answers',
         ])->whereId($id)->first();
